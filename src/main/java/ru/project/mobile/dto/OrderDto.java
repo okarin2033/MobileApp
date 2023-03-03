93d8f05916e675fd7d26d51fdb54c01f4e6d75d6
okarin <palij.jenya2013@gmail.com>
2023-03-03 17:14:43 +0300

    fix

--- src/main/java/ru/project/mobile/dto/OrderDto.java
+++ src/main/java/ru/project/mobile/dto/OrderDto.java
@@ -1,12 +1,16 @@
 package ru.project.mobile.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Map;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
     private Map<String, Integer> bookList;
     private double fullPrice;
